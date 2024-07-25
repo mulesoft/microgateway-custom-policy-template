34aae2e925ad838dd5f2de7e8dace6fb0d19a8e2
JaatadiaMulesoft <35300185+JaatadiaMulesoft@users.noreply.github.com>
2024-07-25 13:38:05 -0300

    W-16238272: Use declarative api for pdk-test. (#75)

--- tests/requests.rs
+++ tests/requests.rs
@@ -5,13 +5,10 @@ mod common;
 use httpmock::MockServer;
 use pdk_test::{pdk_test, TestComposite};
 use pdk_test::port::Port;
-use pdk_test::services::flex::{FlexConfig, Flex};
+use pdk_test::services::flex::{ApiConfig, FlexConfig, Flex, PolicyConfig};
 use pdk_test::services::httpmock::{HttpMockConfig, HttpMock};
 
 use common::*;
-
-// Directory with the configurations for the `hello` test.
-const HELLO_CONFIG_DIR: &str =  concat!(env!("CARGO_MANIFEST_DIR"), "/tests/requests/hello");
 
 // Flex port for the internal test network
 const FLEX_PORT: Port = 8081;
@@ -21,23 +18,35 @@ const FLEX_PORT: Port = 8081;
 #[pdk_test]
 async fn hello() -> anyhow::Result<()> {
 
-    // Configure a Flex service
-    let flex_config = FlexConfig::builder()
-        .version("1.7.0")
-        .hostname("local-flex")
-        .ports([FLEX_PORT])
-        .config_mounts([
-            (POLICY_DIR, "policy"),
-            (COMMON_CONFIG_DIR, "common"),
-            (HELLO_CONFIG_DIR, "hello"),
-        ])
-        .build();
-
     // Configure an HttpMock service
     let httpmock_config = HttpMockConfig::builder()
         .port(80)
         .version("latest")
         .hostname("backend")
+        .build();
+
+    let policy_config = PolicyConfig::builder()
+        .name(POLICY_NAME)
+        .configuration(serde_json::json!({"someProperty": "desiredValue"}))
+        .build();
+
+    let api_config = ApiConfig::builder()
+        .name("myApi")
+        .upstream(&httpmock_config)
+        .path("/anything/echo/")
+        .port(FLEX_PORT)
+        .policies([policy_config])
+        .build();
+
+    // Configure a Flex service
+    let flex_config = FlexConfig::builder()
+        .version("1.7.0")
+        .hostname("local-flex")
+        .with_api(api_config)
+        .config_mounts([
+            (POLICY_DIR, "policy"),
+            (COMMON_CONFIG_DIR, "common"),
+        ])
         .build();
 
     // Compose the services
