dea145200dab29f2508160ae6b691fca8212f56e
Andrés Testi <34962582+andytesti@users.noreply.github.com>
2024-11-26 15:30:23 +0000

    [W-17131145] Merge master into develop branch. (#87)

--- tests/requests.rs
+++ tests/requests.rs
@@ -1,16 +1,14 @@
 // Copyright 2023 Salesforce, Inc. All rights reserved.
 
+mod common;
+
 use httpmock::MockServer;
 use pdk_test::{pdk_test, TestComposite};
 use pdk_test::port::Port;
-use pdk_test::services::flex::{FlexConfig, Flex};
+use pdk_test::services::flex::{ApiConfig, FlexConfig, Flex, PolicyConfig};
 use pdk_test::services::httpmock::{HttpMockConfig, HttpMock};
 
-// Directory where the policies implementations are stored
-const POLICY_DIR: &str =  concat!(env!("CARGO_MANIFEST_DIR"), "/test/config/custom-policies");
-
-// Directory where the tests are configured
-const CONFIG_DIR: &str =  concat!(env!("CARGO_MANIFEST_DIR"), "/tests/config/requests");
+use common::*;
 
 // Flex port for the internal test network
 const FLEX_PORT: Port = 8081;
@@ -18,23 +16,37 @@ const FLEX_PORT: Port = 8081;
 // This integration test shows how to build a test to compose a local-flex instance
 // with a MockServer backend
 #[pdk_test]
-async fn get_request() -> anyhow::Result<()> {
+async fn hello() -> anyhow::Result<()> {
+
+    // Configure an HttpMock service
+    let httpmock_config = HttpMockConfig::builder()
+        .port(80)
+        .version("latest")
+        .hostname("backend")
+        .build();
+
+    let policy_config = PolicyConfig::builder()
+        .name(POLICY_NAME)
+        .configuration(serde_json::json!({"someProperty": "desiredValue"}))
+        .build();
+
+    let api_config = ApiConfig::builder()
+        .name("myApi")
+        .upstream(&httpmock_config)
+        .path("/anything/echo/")
+        .port(FLEX_PORT)
+        .policies([policy_config])
+        .build();
 
     // Configure a Flex service
     let flex_config = FlexConfig::builder()
-        .version("1.5.1")
+        .version("1.7.0")
         .hostname("local-flex")
-        .ports([FLEX_PORT])
+        .with_api(api_config)
         .config_mounts([
             (POLICY_DIR, "policy"),
-            (CONFIG_DIR, "config")
+            (COMMON_CONFIG_DIR, "common"),
         ])
-        .build();
-
-    // Configure an HttpMock service 
-    let httpmock_config = HttpMockConfig::builder()
-        .version("latest")
-        .hostname("backend")
         .build();
 
     // Compose the services
@@ -53,7 +65,7 @@ async fn get_request() -> anyhow::Result<()> {
     // Get a handle to the HttpMock service
     let httpmock: HttpMock = composite.service()?;
 
-    // Create a MockServer 
+    // Create a MockServer
     let mock_server = MockServer::connect_async(httpmock.socket()).await;
 
     // Mock a /hello request
@@ -66,7 +78,7 @@ async fn get_request() -> anyhow::Result<()> {
     let response = reqwest::get(format!("{flex_url}/hello")).await?;
 
     // Assert on the response
-    assert_eq!(response.status(), 200);
+    assert_eq!(response.status(), 202);
 
     Ok(())
 }
